97719b0f462750738f8206063ff043bb3c4dc891
Konstantin <judge.konstantin@gmail.com>
2024-02-23 00:45:31 +0300

    Refactor to FileVisitor

--- _5_JokeFile/src/Main.java
+++ _5_JokeFile/src/Main.java
@@ -5,7 +5,6 @@ import java.nio.file.Paths;
 public class Main {
     public static void main(String[] args) throws IOException {
         Files.walkFileTree(Paths.get("F:\\Java"), new JokeFileVisitor());
-
     }
 }
 
