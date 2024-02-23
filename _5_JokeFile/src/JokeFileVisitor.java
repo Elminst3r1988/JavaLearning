d4333ca51fb3ec2ebfdf4f3c4b83d240a08bf194
Konstantin <judge.konstantin@gmail.com>
2024-02-23 10:34:40 +0300

    try() -> Files.write()

--- _5_JokeFile/src/JokeFileVisitor.java
+++ _5_JokeFile/src/JokeFileVisitor.java
@@ -1,22 +1,19 @@
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 
 public class JokeFileVisitor extends SimpleFileVisitor<Path> {
-    private final byte[] helloWorld = ("public class Main {\n" +
+    private final String HELLO_WORLD_TEXT = "public class Main {\n" +
             "    public static void main(String[] args) throws IOException {\n" +
             "        System.out.println(\"Hello World!\");\n" +
             "    }\n" +
-            "}").getBytes();
-
-    private FileWriter fileWriter;
+            "}";
 
     @Override
     public FileVisitResult preVisitDirectory(Path file, BasicFileAttributes attrs) throws IOException {
         Path fileName = Paths.get(file + File.separator + "joke.java");
-        Files.write(fileName, helloWorld);
+        Files.write(fileName, HELLO_WORLD_TEXT.getBytes());
 
         return FileVisitResult.CONTINUE;
     }
